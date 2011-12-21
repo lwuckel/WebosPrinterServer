febb371cb5d532384bbd7b4fb4d79e45a4b884c3
Lars Wuckel <lars.wuckel@gmail.com>
2011-12-21 16:04:06 +0100

    Commandline message

--- Main.cs
+++ Main.cs
@@ -6,8 +6,8 @@ namespace WebosPrinter
 	{
 		public static void Main (string[] args)
 		{
-			if (args.Length == 0 || args.Length >1)
-				new ArgumentException(@"
+			if (args.Length == 0 || args.Length >1){
+				Console.Out.WriteLine(@"
 Command: WebosPrinterServer <script-file>
 script-file is a shell script that work with pcl-file from WebOS.
 It will be called if a printjob received
@@ -15,6 +15,8 @@ It will be called if a printjob received
 Parameter of the scriptfile call:
 Parameter: pcl-file color-mode plex-mode 
 ");
+				return;
+			}
 			
 			new WebOsPrinterServer(args[0])
 				.Start();
